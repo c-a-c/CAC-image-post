a9a50fc6d7d5b31a4b0aa7bea7b4f69b0ad9dabb
CrazyHuman00 <asakurashi00@icloud.com>
2025-02-14 00:57:50 +0900

    update: とりあえずFirebaseにアップロードできるように

--- backend/db/firebase.go
+++ backend/db/firebase.go
@@ -1,1 +1,41 @@
 package db
+
+import (
+	"context"
+	"log"
+	"os"
+
+	firebase "firebase.google.com/go/v4"
+	"firebase.google.com/go/v4/storage"
+	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
+)
+
+// Firebaseとの接続
+func NewFirebase() (*storage.Client, string, error) {
+	// 環境変数の読み込み
+	if os.Getenv(("GO_ENV")) == "dev" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	// Firebaseの設定
+	ctx := context.Background()
+	credentialsFile := os.Getenv("FIREBASE_ADMIN_SDK")
+    sa := option.WithCredentialsFile(credentialsFile)
+	app, err := firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Storageのクライアントを取得
+	storageClient, err := app.Storage(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	bucketName := os.Getenv("FIREBASE_STORAGE_BUCKET")
+	return storageClient, bucketName, nil
+}
--- backend/model/user.go
+++ backend/model/user.go
@@ -5,6 +5,7 @@ import "time"
 type User struct {
 	ID        uint      `json:"id" gorm:"primary_key"`
 	Username  string    `json:"username" gorm:"unique;not null"`
+	Email	  string    `json:"email" gorm:"unique;not null"`
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 	IsMember  bool      `json:"isMember"`
